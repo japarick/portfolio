5854c023d5305de72c92d7e19d5180114e91daa7
Henrique <henrique_kondo@hotmail.com>
2023-02-16 12:10:44 -0300

    Portfólio
    Meu primeiro projeto.

---
+++ index.js
@@ -0,0 +1,62 @@
+function mostrarPaginaInicial() {
+    document.getElementById("inicio").style.display = 'grid';
+    document.getElementById("formacao").style.display = 'none';
+    document.getElementById("experiencia").style.display = 'none';
+    document.getElementById("habilidades").style.display = 'none';
+    document.getElementById("projetos").style.display = 'none'
+    document.getElementById("contato").style.display = 'none';
+}
+
+function mostrarInicio() {
+    document.getElementById("inicio").style.display = 'grid';
+    document.getElementById("formacao").style.display = 'none';
+    document.getElementById("experiencia").style.display = 'none';
+    document.getElementById("habilidades").style.display = 'none';
+    document.getElementById("projetos").style.display = 'none'
+    document.getElementById("contato").style.display = 'none';
+}
+
+function mostrarFormacao() {
+    document.getElementById("inicio").style.display = 'none';
+    document.getElementById("formacao").style.display = 'grid';
+    document.getElementById("experiencia").style.display = 'none';
+    document.getElementById("habilidades").style.display = 'none';
+    document.getElementById("projetos").style.display = 'none'
+    document.getElementById("contato").style.display = 'none';
+}
+
+function mostrarExperiencia() {
+    document.getElementById("inicio").style.display = 'none';
+    document.getElementById("formacao").style.display = 'none';
+    document.getElementById("experiencia").style.display = 'grid';
+    document.getElementById("habilidades").style.display = 'none';
+    document.getElementById("projetos").style.display = 'none'
+    document.getElementById("contato").style.display = 'none';
+}
+
+function mostrarHabilidades() {
+    document.getElementById("inicio").style.display = 'none';
+    document.getElementById("formacao").style.display = 'none';
+    document.getElementById("experiencia").style.display = 'none';
+    document.getElementById("habilidades").style.display = 'grid';
+    document.getElementById("projetos").style.display = 'none'
+    document.getElementById("contato").style.display = 'none';
+}
+
+function mostrarProjetos() {
+    document.getElementById("inicio").style.display = 'none';
+    document.getElementById("formacao").style.display = 'none';
+    document.getElementById("experiencia").style.display = 'none';
+    document.getElementById("habilidades").style.display = 'none';
+    document.getElementById("projetos").style.display = 'grid'
+    document.getElementById("contato").style.display = 'none';
+}
+
+function mostrarContato() {
+    document.getElementById("inicio").style.display = 'none';
+    document.getElementById("formacao").style.display = 'none';
+    document.getElementById("experiencia").style.display = 'none';
+    document.getElementById("habilidades").style.display = 'none';
+    document.getElementById("projetos").style.display = 'none'
+    document.getElementById("contato").style.display = 'grid';
+}
